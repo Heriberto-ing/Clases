9011278cf4ee223d3bde5c4edf638b9a5764673b
Heriberto-ing <142918003+Heriberto-ing@users.noreply.github.com>
2023-10-02 18:48:36 -0500

    Add files via upload

---
+++ clase 26 de SEP/js/app.js
@@ -0,0 +1,17 @@
+import { personas } from "./persona.js";
+import { computersChoiceDisplay } from "./dom.js";
+
+const sectionMain = document.createElement('div');
+const parrafoPrincipal = document.createElement('p');
+parrafoPrincipal.innerHTML = personas[0].primerNombre + ' ' +personas[0].apellido;
+sectionMain.classList.add("main");
+document.body.appendChild(parrafoPrincipal);
+
+    const seccion = `
+    <section class = "main">
+        <p>Dentro de otro section</p>
+    </section>
+    `;
+
+sectionMain.innerHTML = seccion;
+document.body.appendChild(sectionMain);
---
+++ clase 26 de SEP/js/persona.js
@@ -0,0 +1,5 @@
+export const personas =[
+    {primerNombre: "Juan",apellido:"Fernandez"},
+    {primerNombre: "Miguel",apellido:"Ramirez"},
+    {primerNombre: "Nicoll",apellido:"Ortiz"}
+]
